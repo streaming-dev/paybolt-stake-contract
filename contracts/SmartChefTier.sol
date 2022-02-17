5d3c36851464d6a561b3ef4e25be676097c0ad39
streaming-dev <streaming0214@gmail.com>
2022-02-17 00:57:47 -0500

    feat: updated pool logic with paybolt tier

--- contracts/SmartChefTier.sol
+++ contracts/SmartChefTier.sol
@@ -7,7 +7,6 @@ import "./interfaces/math/SafeMath.sol";
 import "./interfaces/ReentrancyGuard.sol";
 import "./interfaces/OwnableUpgradeable.sol";
 import "./interfaces/IToken.sol";
-import "./interfaces/IAutoCompoundPool.sol";
 
 contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
     using SafeMath for uint;
@@ -23,11 +22,12 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
     // Info of each pool.
     struct PoolInfo {
         uint totalSupply;
-        uint allocPoint;       // How many allocation points assigned to this pool. REVAs to distribute per block.
+        uint allocPoint;       // How many allocation points assigned to this pool. PAYs to distribute per block.
+        uint minStakeAmount;   // How many tokens are deposited minimum
         uint timeLocked;       // How long stake must be locked for
-        uint lastRewardBlock;  // Last block number that REVAs distribution occurs.
-        uint accPayboltPerShare; // Accumulated REVAs per share, times 1e12. See below.
-        uint accPayboltPerShareFromFees; // Accumulated REVAs per share, times 1e12. See below.
+        uint lastRewardBlock;  // Last block number that PAYs distribution occurs.
+        uint accPayboltPerShare; // Accumulated PAYs per share, times 1e12. See below.
+        uint accPayboltPerShareFromFees; // Accumulated PAYs per share, times 1e12. See below.
         uint lastAccPayboltFromFees; // last recorded total accumulated paybolt from fees
     }
 
@@ -52,10 +52,6 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
     uint public constant EARLY_WITHDRAWAL_FEE_PRECISION = 1000000;
     uint public constant MAX_EARLY_WITHDRAWAL_FEE = 500000;
 
-    // variables for autocompounding upgrade
-    address public payboltAutoCompoundPool;
-    mapping (uint => mapping (address => bool)) public userIsCompounding;
-
     event Deposit(address indexed user, uint indexed pid, uint amount);
     event Withdraw(address indexed user, uint indexed pid, uint amount);
     event EarlyWithdrawal(address indexed user, uint indexed pid, uint amount, uint withdrawalFee);
@@ -64,9 +60,9 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
     event PoolAdded(uint allocPoint, uint timeLocked);
     event SetPayboltPerBlock(uint payboltPerBlock);
     event SetEarlyWithdrawalFee(uint earlyWithdrawalFee);
-    event SetPool(uint pid, uint allocPoint);
-    event SetPayboltAutoCompoundPool(address _payboltAutoCompoundPool);
-    event CompoundingEnabled(address indexed user, uint pid, bool enabled);
+    event SetPoolAllocPoint(uint pid, uint allocPoint);
+    event SetPoolMinStake(uint pid, uint minStakeAmount);
+    event SetPoolTimeLocked(uint pid, uint timeLocked);
 
     function initialize(
         address _payboltToken,
@@ -86,8 +82,9 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         // staking pool
         poolInfo.push(PoolInfo({
             totalSupply: 0,
-            allocPoint: 1000,
-            timeLocked: 0 days,
+            allocPoint: 12000,
+            minStakeAmount: uint(300).mul(IBEP20(payboltToken).decimals()),
+            timeLocked: 90 days,
             lastRewardBlock: startBlock,
             accPayboltPerShare: 0,
             accPayboltPerShareFromFees: 0,
@@ -95,17 +92,9 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         }));
         poolInfo.push(PoolInfo({
             totalSupply: 0,
-            allocPoint: 2000,
-            timeLocked: 7 days,
-            lastRewardBlock: startBlock,
-            accPayboltPerShare: 0,
-            accPayboltPerShareFromFees: 0,
-            lastAccPayboltFromFees: 0
-        }));
-        poolInfo.push(PoolInfo({
-            totalSupply: 0,
-            allocPoint: 3000,
-            timeLocked: 30 days,
+            allocPoint: 8000,
+            minStakeAmount: uint(150).mul(IBEP20(payboltToken).decimals()),
+            timeLocked: 90 days,
             lastRewardBlock: startBlock,
             accPayboltPerShare: 0,
             accPayboltPerShareFromFees: 0,
@@ -114,27 +103,33 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         poolInfo.push(PoolInfo({
             totalSupply: 0,
             allocPoint: 4000,
+            minStakeAmount: uint(50).mul(IBEP20(payboltToken).decimals()),
             timeLocked: 90 days,
             lastRewardBlock: startBlock,
             accPayboltPerShare: 0,
             accPayboltPerShareFromFees: 0,
             lastAccPayboltFromFees: 0
         }));
-        totalAllocPoint = 10000;
+        poolInfo.push(PoolInfo({
+            totalSupply: 0,
+            allocPoint: 1000,
+            minStakeAmount: uint(5).mul(IBEP20(payboltToken).decimals()),
+            timeLocked: 90 days,
+            lastRewardBlock: startBlock,
+            accPayboltPerShare: 0,
+            accPayboltPerShareFromFees: 0,
+            lastAccPayboltFromFees: 0
+        }));
+        totalAllocPoint = 25000;
     }
 
     function poolLength() external view returns (uint) {
         return poolInfo.length;
     }
 
-    modifier payboltAutoCompoundPoolOnly {
-        require(msg.sender == payboltAutoCompoundPool, "AUTO COMPOUND POOL ONLY");
-        _;
-    }
-
     /* ========== External Functions ========== */
 
-    // View function to see pending REVAs from Pools on frontend.
+    // View function to see pending PAYs from Pools on frontend.
     function pendingPaybolt(uint _pid, address _user) external view returns (uint) {
         PoolInfo memory pool = poolInfo[_pid];
         UserPoolInfo memory user = userPoolInfo[_pid][_user];
@@ -200,11 +195,12 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         }
     }
 
-    // Deposit REVA tokens for REVA allocation.
+    // Deposit PAY tokens for PAY allocation.
     function deposit(uint _pid, uint _amount) external nonReentrant {
-        require(!userIsCompounding[_pid][msg.sender], "Can't deposit when compounding");
         PoolInfo storage pool = poolInfo[_pid];
         UserPoolInfo storage user = userPoolInfo[_pid][msg.sender];
+        require(_amount + user.amount > pool.minStakeAmount, "Should be greater then minimum stake amount");
+
         updatePool(_pid);
         if (user.amount > 0) {
             _claimPendingMintReward(_pid, msg.sender);
@@ -228,6 +224,7 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
     function withdraw(uint _pid, uint _amount) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserPoolInfo storage user = userPoolInfo[_pid][msg.sender];
+        require(user.amount - _amount > pool.minStakeAmount, "Should be greater then minimum stake amount");
         require(user.amount >= _amount, "withdraw: not good");
         require(block.timestamp >= user.timeDeposited.add(pool.timeLocked), "time locked");
 
@@ -309,100 +306,16 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         }
     }
 
-    function enterCompoundingPosition(uint _pid, address _user) external nonReentrant payboltAutoCompoundPoolOnly {
-        updatePool(_pid);
-        PoolInfo storage pool = poolInfo[_pid];
-        UserPoolInfo storage user = userPoolInfo[_pid][_user];
-        UserPoolInfo storage payboltAutoCompoundInfo = userPoolInfo[_pid][payboltAutoCompoundPool];
-        uint migrationAmount = user.amount;
-
-        if (user.amount > 0) {
-            _claimPendingMintReward(_pid, _user);
-            _claimPendingFeeReward(_pid, _user);
-        }
-        if (payboltAutoCompoundInfo.amount > 0) {
-            _claimPendingMintReward(_pid, payboltAutoCompoundPool);
-            _claimPendingFeeReward(_pid, payboltAutoCompoundPool);
-        }
-
-        user.amount = 0;
-        payboltAutoCompoundInfo.amount = payboltAutoCompoundInfo.amount.add(migrationAmount);
-        payboltAutoCompoundInfo.rewardDebt = payboltAutoCompoundInfo.amount.mul(pool.accPayboltPerShare).div(1e12);
-        payboltAutoCompoundInfo.rewardFeeDebt = payboltAutoCompoundInfo.amount.mul(pool.accPayboltPerShareFromFees).div(1e12);
-        payboltAutoCompoundInfo.timeDeposited = block.timestamp;
-
-        userIsCompounding[_pid][_user] = true;
-        emit CompoundingEnabled(_user, _pid, true);
-    }
-
-    function exitCompoundingPosition(uint _pid, uint _amount, address _user) external nonReentrant payboltAutoCompoundPoolOnly {
-        updatePool(_pid);
-        PoolInfo storage pool = poolInfo[_pid];
-        UserPoolInfo storage user = userPoolInfo[_pid][_user];
-        UserPoolInfo storage payboltAutoCompoundInfo = userPoolInfo[_pid][payboltAutoCompoundPool];
-
-        if (user.amount > 0) {
-            _claimPendingMintReward(_pid, _user);
-            _claimPendingFeeReward(_pid, _user);
-        }
-        if (payboltAutoCompoundInfo.amount > 0) {
-            _claimPendingMintReward(_pid, payboltAutoCompoundPool);
-            _claimPendingFeeReward(_pid, payboltAutoCompoundPool);
-        }
-
-        payboltAutoCompoundInfo.amount = payboltAutoCompoundInfo.amount.sub(_amount);
-        payboltAutoCompoundInfo.rewardDebt = payboltAutoCompoundInfo.amount.mul(pool.accPayboltPerShare).div(1e12);
-        payboltAutoCompoundInfo.rewardFeeDebt = payboltAutoCompoundInfo.amount.mul(pool.accPayboltPerShareFromFees).div(1e12);
-
-        user.amount = user.amount.add(_amount);
-        user.rewardDebt = user.amount.mul(pool.accPayboltPerShare).div(1e12);
-        user.rewardFeeDebt = user.amount.mul(pool.accPayboltPerShareFromFees).div(1e12);
-        user.timeDeposited = block.timestamp;
-
-        userIsCompounding[_pid][_user] = false;
-        emit CompoundingEnabled(_user, _pid, false);
-    }
-
-    function depositToCompoundingPosition(uint _pid, uint _amount) external {
-        updatePool(_pid);
-        PoolInfo storage pool = poolInfo[_pid];
-        UserPoolInfo storage user = userPoolInfo[_pid][msg.sender];
-        UserPoolInfo storage payboltAutoCompoundInfo = userPoolInfo[_pid][payboltAutoCompoundPool];
-
-        require(user.amount == 0, "Can't compound when deposited");
-        require(_amount > 0, "Must deposit non zero amount");
-
-        if (payboltAutoCompoundInfo.amount > 0) {
-            _claimPendingMintReward(_pid, payboltAutoCompoundPool);
-            _claimPendingFeeReward(_pid, payboltAutoCompoundPool);
-        }
-
-        uint before = IBEP20(payboltToken).balanceOf(address(this));
-        IBEP20(payboltToken).safeTransferFrom(address(msg.sender), address(this), _amount);
-        uint post = IBEP20(payboltToken).balanceOf(address(this));
-        uint finalAmount = post.sub(before);
-
-        payboltAutoCompoundInfo.amount = payboltAutoCompoundInfo.amount.add(finalAmount);
-        payboltAutoCompoundInfo.timeDeposited = block.timestamp;
-        payboltAutoCompoundInfo.rewardDebt = payboltAutoCompoundInfo.amount.mul(pool.accPayboltPerShare).div(1e12);
-        payboltAutoCompoundInfo.rewardFeeDebt = payboltAutoCompoundInfo.amount.mul(pool.accPayboltPerShareFromFees).div(1e12);
-
-        pool.totalSupply = pool.totalSupply.add(finalAmount);
-
-        userIsCompounding[_pid][msg.sender] = true;
-
-        IAutoCompoundPool(payboltAutoCompoundPool).notifyDeposited(_pid, finalAmount, msg.sender);
-    }
-
     // Add a new lp to the pool. Can only be called by the owner.
     // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
-    function add(uint _allocPoint, uint _timeLocked) external onlyOwner {
+    function addPool(uint _allocPoint, uint _minStakeAmount, uint _timeLocked) external onlyOwner {
         massUpdatePools();
         uint lastRewardBlock = block.number > startBlock ? block.number : startBlock;
         totalAllocPoint = totalAllocPoint.add(_allocPoint);
         poolInfo.push(PoolInfo({
             totalSupply: 0,
             allocPoint: _allocPoint,
+            minStakeAmount: _minStakeAmount,
             timeLocked: _timeLocked,
             lastRewardBlock: lastRewardBlock,
             accPayboltPerShare: 0,
@@ -412,15 +325,30 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         emit PoolAdded(_allocPoint, _timeLocked);
     }
 
-    // Update the given pool's REVA allocation point. Can only be called by the owner.
-    function set(uint _pid, uint _allocPoint) external onlyOwner {
+    // Update the given pool's PAY allocation point. Can only be called by the owner.
+    function setAllocPoint(uint _pid, uint _allocPoint) external onlyOwner {
         massUpdatePools();
         uint prevAllocPoint = poolInfo[_pid].allocPoint;
         poolInfo[_pid].allocPoint = _allocPoint;
         if (prevAllocPoint != _allocPoint) {
             totalAllocPoint = totalAllocPoint.sub(prevAllocPoint).add(_allocPoint);
         }
-        emit SetPool(_pid, _allocPoint);
+        emit SetPoolAllocPoint(_pid, _allocPoint);
+    }
+
+    // Update the given pool's PAY minimum staked amount. Can only be called by the owner.
+    function setMinStake(uint _pid, uint _minStakeAmount) external onlyOwner {
+        require(_minStakeAmount > 0, "Should be greater zero!");
+        massUpdatePools();
+        poolInfo[_pid].minStakeAmount = _minStakeAmount;
+        emit SetPoolMinStake(_pid, _minStakeAmount);
+    }
+
+    // Update the given pool's PAY locked time. Can only be called by the owner.
+    function setTimeLocked(uint _pid, uint _timeLocked) external onlyOwner {
+        massUpdatePools();
+        poolInfo[_pid].timeLocked = _timeLocked;
+        emit SetPoolTimeLocked(_pid, _timeLocked);
     }
 
     function setPayboltPerBlock(uint _payboltPerBlock) external onlyOwner {
@@ -432,11 +360,6 @@ contract PayboltStakingPool is OwnableUpgradeable, ReentrancyGuard {
         require(_earlyWithdrawalFee <= MAX_EARLY_WITHDRAWAL_FEE, "MAX_EARLY_WITHDRAWAL_FEE");
         earlyWithdrawalFee = _earlyWithdrawalFee;
         emit SetEarlyWithdrawalFee(earlyWithdrawalFee);
-    }
-
-    function setPayboltAutoCompoundPool(address _payboltAutoCompoundPool) external onlyOwner {
-        payboltAutoCompoundPool = _payboltAutoCompoundPool;
-        emit SetPayboltAutoCompoundPool(_payboltAutoCompoundPool);
     }
 
     function _claimPendingMintReward(uint _pid, address _user) private {
